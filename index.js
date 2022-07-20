8a0f465672bb3ed2a9f3c88e9a941e325f28008b
ashish-careercarve <104759136+ashish-careercarve@users.noreply.github.com>
2022-07-20 12:21:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
- 
+const db = require('./db-car');
+
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -8,6 +13,18 @@ app.get("/", (req, res) => {
 app.get("/api/cources", (req, res) => {
   res.send([1, 2, 3]);
 });
+
+
+app.post("/student", (req,res) => {
+  const qu= 'INSERT INTO student (first_name, last_name, ph_no, email, address, li_url) VALUES (?)'
+  const va= "${req.body.first_name}, ${req.body.last_name}, ${req.body.ph_no}, ${req.body.email}, ${req.body.address}, ${req.body.li_url}"
+  res.send(va)
+  db.query(qu, va, (err,result)=>{
+    if(err) {
+    console.log(err)
+    } 
+  res.send(result)
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
